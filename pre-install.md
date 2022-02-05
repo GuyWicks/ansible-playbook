73539be78a93cc19a782fe972615d10cf07655ba
Guy Wicks <guy.wicks@gmail.com>
2022-02-05 15:43:13 +0000

    Check root, update and iptables

---
+++ pre-install.md
@@ -0,0 +1,5 @@
+# Preinstall
+
+- Open any required inbound ports for install commissioning
+- PORT 80 (nginx)
+
--- pre-install.sh
+++ pre-install.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
-python3 --version
+# Prebuild - root 
+
+
+# https://github.com/GuyWicks/anisble-playbook.git
+
+# Installer
+# curl -s https://raw.githubusercontent.com/GuyWicks/anisble-playbook/guy-prebuild/pre-install.sh | sh
+
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+
+apt get update && apt get install -y
+
+# Allow HTTP/HTTP Inbound
+sudo iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+#sudo iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+sudo iptables -F
+
+
+
+#python3 --version
