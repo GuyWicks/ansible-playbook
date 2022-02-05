f2b2e1f9864c6f93e7384375de36d8538b463ace
Guy Wicks <guy.wicks@gmail.com>
2022-02-05 20:23:31 +0000

    Move certbot to own script

--- pre-install.md
+++ pre-install.md
@@ -1,5 +1,9 @@
 # Preinstall
 
-- Open any required inbound ports for install commissioning
-- PORT 80 (nginx)
+- Instance upgrade (apt update && dist-update)
+- Open any required inbound ports for install commission testing
+- PORT 80 HTTP and 443 HTTPS
+- Install nginx
 
+## Note
+Run as `root` 
--- pre-install.sh
+++ pre-install.sh
@@ -17,6 +17,10 @@ fi
 # Update the instance
 apt update && apt dist-upgrade -y
 apt install nginx 
+snap install core
+snap refresh core
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
 
 # Allow HTTP/HTTP Inbound
 sudo iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
